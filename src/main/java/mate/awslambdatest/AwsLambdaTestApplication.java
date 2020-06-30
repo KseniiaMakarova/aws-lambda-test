ddb31f1fe3625f1c26a74f74eec9cd196674fb8f
Kseniia Makarova <kseniia.makarova.kyiv@gmail.com>
2020-06-30 11:34:35 +0300

    project setup

---
+++ src/main/java/mate/awslambdatest/AwsLambdaTestApplication.java
@@ -0,0 +1,13 @@
+package mate.awslambdatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsLambdaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsLambdaTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mate/awslambdatest/AwsLambdaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.awslambdatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsLambdaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
